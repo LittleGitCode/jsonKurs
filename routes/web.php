a8008acd9dde3b1b5e7677e020b32ba5ca7c7665
LittleGitCode <misimas25@gmail.com>
2023-11-30 03:48:01 +0700

    change route to api

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\KursController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/kurs', [KursController::class, 'jsonDollar']);
 
 
